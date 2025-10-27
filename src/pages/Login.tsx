c890afefa88180fddf28b11029c91bd4039c88fe
Kcandy <83863207+KCandy24@users.noreply.github.com>
2025-10-27 16:35:34 +0800

    feat(Login Page (Frontend) Fixes #10): Added form inputs for Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,22 @@
 import Card from "@components/Card"
+import Logo from "@components/Logo"
+import { Email, Password } from "@components/Forms";
+
+import { useState } from "react"
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
-    <div className="flex justify-center ">
+    <div className="flex justify-center items-center">
       <Card style="flex flex-col m-5">
-        <span>Delta</span>
+        <Logo size="lg"/>
         <span>Welcome Back!</span>
+          <Email value={email} setValue={setEmail} size="lg"/>
+          <Password value={password} setValue={setPassword} size="lg"/>
       </Card>
+
     </div>
   )
 }
