8c88786a25dcc94989f50df592f6eca6efbf9e8b
Kcandy <83863207+KCandy24@users.noreply.github.com>
2025-11-02 23:58:06 +0800

    init: Apply playwright

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: 1,
+  use: {
+    baseURL: 'http://localhost:8080',
+    headless: true,
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'bun run dev',
+    port: 8080,
+    reuseExistingServer: !process.env.CI,
+  },
+});
